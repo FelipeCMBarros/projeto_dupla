54f47c8b2642fefeb4e3b45aeb7b185c62a8722c
BernardoSafira <bernardosafira@gmail.com>
2025-05-05 14:40:32 -0300

    correção do exercício 2

--- funcoes.py
+++ funcoes.py
@@ -9,11 +9,11 @@ def rolar_dados(dados):
   return lista_dados
 
 def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
-  for dado_para_guardar in dados_rolados:
-    if len(dados_no_estoque) < 4:
-      dados_no_estoque.append(dados_rolados[dado_para_guardar])
-      dados_rolados.remove(dados_rolados[dado_para_guardar])
-
-  lista = [dados_rolados, dados_no_estoque]
-  return lista
+    dado = dados_rolados[dado_para_guardar]
+    
+    dados_no_estoque.append(dado)
+    
+    del dados_rolados[dado_para_guardar]
+    
+    return [dados_rolados, dados_no_estoque]
 
